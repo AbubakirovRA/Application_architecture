4fd73042d7769de0391084a29f9edcf5f2ac4247
AbubakirovRA <AbubakirovRA@ya.ru>
2022-11-01 11:46:37 +0500

    Initial commit

---
+++ WP3/eample1.java
@@ -0,0 +1,35 @@
+class Praktikum {
+    public static void main(String[] args) {
+
+        double moneyBeforeSalary = 16000.0; // Количество денег до зарплаты
+        int daysBeforeSalary = 18;
+
+        if (moneyBeforeSalary < 3000) {
+            System.out.println("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!");
+        } else if (moneyBeforeSalary < 10000) {
+            // Здесь нужное условие уже добавили
+            if (daysBeforeSalary < 10) {
+                System.out.println("Окей, пора в Макдак!");
+            } else {
+                System.out.println("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!");
+            }
+            
+        } else if (moneyBeforeSalary < 15000) {
+			// Допишите условие
+            if (daysBeforeSalary < 17) {
+                System.out.println("Неплохо! Прикупите долларов и зайдите поужинать в классное место.");
+            } else {
+                System.out.println("Окей, пора в Макдак!");
+            }
+
+        } else {
+			// Если до зарплаты меньше 10 дней, то едим крабов
+            if (daysBeforeSalary < 10) {
+            System.out.println("Класс! Заказывайте крабов!");
+            // Иначе — доллары и ужин в хорошем ресторане
+            } else {
+            System.out.println("Неплохо! Прикупите долларов и зайдите поужинать в классное место.");
+            }
+        }
+    }
+}
